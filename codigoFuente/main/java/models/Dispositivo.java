d95e8bfa63854828228c1a27ce55ecf562057d6d
lobocarolinael36-boop <lobo.carolina.el36@gmail.com>
2025-08-05 17:23:02 -0300

    Update Dispositivo.java

--- codigoFuente/main/java/models/Dispositivo.java
+++ codigoFuente/main/java/models/Dispositivo.java
@@ -3,5 +3,15 @@ package modelos;
 public class Dispositivo {
     private String ip;
     private String nombre;
-    // ... añade getters y setters
+    private boolean activo;
+    
+    // Getters y Setters
+    public String getIp() { return ip; }
+    public void setIp(String ip) { this.ip = ip; }
+    
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    
+    public boolean isActivo() { return activo; }
+    public void setActivo(boolean activo) { this.activo = activo; }
 }
